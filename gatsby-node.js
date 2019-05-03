13f066aa57fba41838ba72c8852e5bdf0a2f026a
Leo Bi (Mac Pro) <biminglei@gmail.com>
2019-05-03 09:46:44 +0800

    update

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,3 +3,17 @@ exports.onCreateNode = require('./gatsby/onCreateNode');
 exports.createPages = require('./gatsby/CreatePages');
 
 exports.onCreateWebpackConfig = require('./gatsby/onCreateWebpackConfig');
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  const timestamp = Date.now();
+  if (stage === 'build-javascript') {
+    config.merge({
+      output: {
+        filename: `[name]-${timestamp}-[chunkhash].js`,
+        chunkFilename: `[name]-${timestamp}-[chunkhash].js`,
+      },
+    });
+  }
+
+  return config;
+};
