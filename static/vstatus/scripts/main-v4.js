b0fe22dd0f89a43372d671df2454747be48cb598
Mac <biminglei@gmail.com>
2022-02-24 20:56:13 +0800

    update

--- static/vstatus/scripts/main-v4.js
+++ static/vstatus/scripts/main-v4.js
@@ -30,7 +30,9 @@ window.onload = function onLoad() {
 	bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
 	bar.text.style.fontSize = '1rem';
 	
-	fetch("https://cors-anywhere.herokuapp.com/https://justmysocks5.net/members/getbwcounter.php?service=463251&id=3f405ae0-e4f6-4ab5-8ca9-0bfe0717ed86", {
+	// https://cors-anywhere.herokuapp.com
+	
+	fetch("http://leobi.synology.me:8088/https://justmysocks5.net/members/getbwcounter.php?service=463251&id=3f405ae0-e4f6-4ab5-8ca9-0bfe0717ed86", {
 		"method": "GET",
 		"headers": {
 			"origin": "null"
