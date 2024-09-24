ab98303e18765cf222dc8185d3c3ec5b4acdc316
Leo Bi <biminglei@gmail.com>
2024-09-24 12:39:02 +0800

    update baidu site meta

--- src/html.js
+++ src/html.js
@@ -13,6 +13,7 @@ const HTML = ({
     <head>
       <meta charSet="utf-8" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="baidu-site-verification" content="codeva-9aEYbTyyrW" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
